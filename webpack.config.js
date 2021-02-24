c37c745dedecd693b181b5cf792fdfafabcc3f35
Diem-Quynh <Diem-Quynh@users.noreply.github.com>
2021-02-24 23:44:35 +0700

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,11 @@ module.exports = {
                         "plugins": ["@babel/plugin-proposal-object-rest-spread", "@babel/plugin-proposal-class-properties"]
                     }
                 }
-            }
+            },
+            { 
+                test: /\.ts$/, 
+                use: 'ts-loader', exclude: /node_modules/ 
+            },
         ]
     },
     resolve: {
