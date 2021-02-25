b8e4b55edcb6b3e229d1fa83629d91023c77d056
Diem-Quynh <Diem-Quynh@users.noreply.github.com>
2021-02-25 09:51:24 +0700

    layout /

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,6 @@
-import Link from 'next/link'
-import {route} from 'themes/route'
 import Layout from 'themes/layouts/Admin'
-import { DatePicker, message } from 'antd';
-
-const PostLink = (props) => (
- <li>
-   <Link href="/login">
-     <a>{props.title}</a>
-   </Link>
- </li>
-)
 
 export default () => (
   <Layout>
-    <h1>Dashboard</h1>
-    <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js"/>
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit1"/>
-      <DatePicker />
-    </ul>
   </Layout>
 )
